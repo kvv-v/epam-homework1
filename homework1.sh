24993c9939cdbaa3f99a5b119f984ee2e0ea043a
kvv-v <1234funnyadress@protonmail.com>
2022-01-16 21:03:33 +0500

    init

---
+++ homework1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo '$interfaceName:$IPaddress:$default'
+default=`ip r | grep default | awk '{print $5}'`
+for interface in $(ip a | grep '<' | awk '{print substr($2, 1, length($2)-1)}')
+do
+	echo -n $interface:$(ip address show $interface | grep inet | grep -v inet6 | awk '{print $2}')
+	if [ $default = $interface ] 
+	then
+		echo :default
+	else
+		echo
+	fi
+done
+
